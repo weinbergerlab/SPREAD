08de6cd58f96151199c8e399a2ba9aaa1be5e008
Gigi112 <709141475@qq.com>
2022-06-20 10:17:35 +0200

    add tabpanel

--- shiny app/UI.R
+++ shiny app/UI.R
@@ -31,112 +31,128 @@ ui <- fluidPage(
                
                setSliderColor(c(rep("#b2df8a", 3)), sliderId=c(8,9,10)),
                h4(div(HTML("<em>Set fixed parameters...</em>"))),
-    sliderInput("B_m",
-                "Per capita births rate (months):",
-                min = 0, max = 0.001, value = 0.0008, step=0.0001
-    ),
-    sliderInput("infdur1",
-                "Infectious duration of the 1st infection (days):",
-                min = 1, max = 30, value = 10
-    ),
-    sliderInput("infdur2",
-                "Infectious duration of the 2st infection (days):",
-                min = 1, max = 30, value = 7
-    ),
-    sliderInput("infdur3",
-                "Infectious duration of the 3st infection (days):",
-                min = 1, max = 30, value = 5
-    ),
-    sliderInput("rr1",
-                "Relative risk of infection following 1st infection:",
-                min = 0, max = 1, value = 0.76
-    ),
-    sliderInput("rr2",
-                "Relative risk of infection following 2st infection:",
-                min = 0, max = 1, value = 0.6
-    ),
-    sliderInput("rr3",
-                "Relative risk of infection following 3rd infection:",
-                min = 0, max = 1, value = 0.4
-    ),
-    sliderInput("rho1",
-                "Relative infectiousness for the 2nd infection:",
-                min = 0, max = 1, value = 0.75
-    ),
-    sliderInput("rho2",
-                "Relative infectiousness for the subsequent infections:",
-                min = 0, max = 1, value = 0.51
-    ),
-    
-    br(),
-    
+               sliderInput("B_m",
+                           "Per capita births rate (months):",
+                           min = 0, max = 0.001, value = 0.0008, step=0.0001
+               ),
+               sliderInput("infdur1",
+                           "Infectious duration of the 1st infection (days):",
+                           min = 1, max = 30, value = 10
+               ),
+               sliderInput("infdur2",
+                           "Infectious duration of the 2st infection (days):",
+                           min = 1, max = 30, value = 7
+               ),
+               sliderInput("infdur3",
+                           "Infectious duration of the 3st infection (days):",
+                           min = 1, max = 30, value = 5
+               ),
+               sliderInput("rr1",
+                           "Relative risk of infection following 1st infection:",
+                           min = 0, max = 1, value = 0.76
+               ),
+               sliderInput("rr2",
+                           "Relative risk of infection following 2st infection:",
+                           min = 0, max = 1, value = 0.6
+               ),
+               sliderInput("rr3",
+                           "Relative risk of infection following 3rd infection:",
+                           min = 0, max = 1, value = 0.4
+               ),
+               sliderInput("rho1",
+                           "Relative infectiousness for the 2nd infection:",
+                           min = 0, max = 1, value = 0.75
+               ),
+               sliderInput("rho2",
+                           "Relative infectiousness for the subsequent infections:",
+                           min = 0, max = 1, value = 0.51
+               ),
+               
+               br(),
+               
         ),
-    column(width=6,
-           
-           h4(div(HTML("<em>Set transmission parameters...</em>"))),
-           sliderInput("R0",
-                       "Basic reproductive number (R0, # persons):",
-                       min = 0, max = 20, value = 10.035, step=0.01
-           ),
-           sliderInput("contactrate",
-                       "Average rate of contact between susceptible and infected individuals (per week):",
-                       min = .5, max = 20, value = 10
-           ),
-           sliderInput("amp",
-                       "Amplitude of transmission seasonality:",
-                       min = 0, max = 2, value = 0.35, step=0.01
-           ),
-           sliderInput("phase",
-                       "Peak timing of transmission seasonality:",
-                       min = 0, max = 3.14, value = 1.53, step=0.01
-           ),
-           sliderInput("DurationMatImmunityDays",
-                       "Duration of maternal immunity (days):",
-                       min = 0, max = 150, value = 90, step=1
-           ),
-           sliderInput("virus_import",
-                       "Time at virus importation:",
-                       min = 159, max = 171, value = 159, step=1
-           ),
-           sliderInput("I_ex",
-                       "Amount of virus importation (per 1 million people per week):",
-                       min = 0, max = 100, value = 0, step=5
-           ),
-           hr(),
-           
-    )
+        column(width=6,
+               
+               h4(div(HTML("<em>Set transmission parameters...</em>"))),
+               sliderInput("R0",
+                           "Basic reproductive number (R0, # persons):",
+                           min = 0, max = 20, value = 10.035, step=0.01
+               ),
+               sliderInput("contactrate",
+                           "Average rate of contact between susceptible and infected individuals (per week):",
+                           min = .5, max = 20, value = 10
+               ),
+               sliderInput("amp",
+                           "Amplitude of transmission seasonality:",
+                           min = 0, max = 2, value = 0.35, step=0.01
+               ),
+               sliderInput("phase",
+                           "Peak timing of transmission seasonality:",
+                           min = 0, max = 3.14, value = 1.53, step=0.01
+               ),
+               sliderInput("DurationMatImmunityDays",
+                           "Duration of maternal immunity (days):",
+                           min = 0, max = 150, value = 90, step=1
+               ),
+               hr(),
+               
+        )
       ),
-    h4(div(HTML("<em>Set simulation values...</em>"))),
-    fluidRow(
-    column(width=6,
-           numericInput("popsize", div(HTML("Population size (millions):")), value=10, max=300, min=1, step=1)
-    ),
-    column(width=6,
-           numericInput("pinf","Initial # infected:",value = 100, min = 1, step = 1)
-    )),
-    #br(),
-    
-    sliderInput("timeframe", div(HTML("Maximum time")),0, 600, 318, step=10, post=" weeks"),
-    checkboxInput("lockdown", "Lockdown", FALSE), 
-    width=4
+      h4(div(HTML("<em>Set simulation values...</em>"))),
+      fluidRow(
+        column(width=6,
+               numericInput("popsize", div(HTML("Population size (millions):")), value=10, max=300, min=1, step=1)
+        ),
+        column(width=6,
+               numericInput("pinf","Initial # infected:",value = 100, min = 1, step = 1)
+        )),
+      #br(),
+      
+      sliderInput("timeframe", div(HTML("Maximum time")),0, 600, 318, step=10, post=" weeks"),
+      checkboxInput("lockdown", "Lockdown", FALSE), 
+      width=4
     ),
     
     mainPanel(
-      fluidPage(
-      fluidRow(
-        h3("Predicted RSV cases by clinical outcome"),
-        p(HTML("Simulate the natural course of RSV epidemic in a single population without any interventions.")),
-        plotlyOutput("distPlot")
-        ),
-      br(),
-      fluidRow(
-        # Dynamic valueBoxes
-        valueBoxOutput("progressBox", width = 3),
-        valueBoxOutput("approvalBox", width = 3),
-        valueBoxOutput("BRRBox", width = 3)
-      ),
-      br(),
-      br()
-    )
-  )) 
-  )
+      
+      navbarPage("Output:",
+                 
+                 tabPanel("Seasonal Epidemics",
+                          fluidPage(
+                            fluidRow(
+                              h3("Seasonal RSV Epidemics without interuption"),
+                              p(HTML("Simulate the natural course of RSV epidemic in Dutch population without any interventions.")),
+                              plotlyOutput("Plot_Normal")
+                            ),
+                            br())),
+                 tabPanel("Re-emergent Epidemics",
+                          fluidPage(
+                            fluidRow(
+                              h3("Simulated RSV activites after re-introduction"),
+                              p(HTML("Simulate the change in the time course of RSV cases after intervention and virus re-introduction")),
+                              plotlyOutput("Plot_Intervention"),
+                              br(),
+                              br(),
+                              wellPanel(
+                                h4(div(HTML("<em>Set intervention parameters...</em>"))),
+                                column(width=6,
+                                       numericInput("Tint","Duration of Intervention (weeks):",value = 52, min = 0, step = 1)
+                                ),
+                                column(width=6,
+                                       numericInput("virus_import",
+                                                    "Time at virus importation (weeks):",value = 53, min = 0, step=1)),
+                                sliderInput("I_ex",
+                                            "Amount of virus importation (per 1 million people per week):",
+                                            min = 0, max = 100, value = 0, step=5
+                                ),
+                                p(HTML("<b>Intervention type: reducing transmission, </b> for example via social distancing or travel restriction.")),
+                                sliderInput("s1", "Reduction in transmission from infections ", 0, 100, 30, pre="%",step=1, animate=TRUE),
+                                p(HTML("<b>User instructions:</b> The graph shows the expected numbers of individuals over time who are infected over time, with and without an intervention, with and without virus importation. A more detailed description of the model is provided in the Model Description tab. The population size, initial condition, and parameter values used to simulate the spread of infection can be specified through the sliders located in the left-hand panel. Default slider values are equal to estimates taken from the literature (see Sources tab). The strength and timing of the intervention is controlled by the sliders below the plot."))
+                              )
+                            )
+                          )),
+                 br(),
+                 br()
+      )
+    )) 
+)
--- shiny app/server_new.R
+++ shiny app/server_new.R
@@ -15,8 +15,9 @@ library("shinydashboard")
 # Define server 
 #
 server <- function(input, output) {
+  
   # Create reactive input
-  dataInput <- reactive({
+  simMSIS_Normal <- reactive({
     init       <-
       c(M=0,
         S0 =  input$popsize*1000000-input$pinf,
@@ -30,7 +31,60 @@ server <- function(input, output) {
       )
     ## beta: infection parameter; gamma: duration of infectiousness
     length.step=7 # weekly
-    parameters <-
+    
+    parameters_Normal <-
+      c(beta = input$R0/(input$infdur1/length.step)/input$contactrate,
+        b1=input$amp,
+        phi=input$phase,
+        gamma1 = 1 / (input$infdur1/length.step),
+        gamma2 = 1 / (input$infdur2/length.step),
+        gamma3 = 1 / (input$infdur3/length.step),
+        gamma4 = 1 / (input$infdur3/length.step),
+        rho1=input$rho1,
+        rho2=input$rho2,
+        sigma1=input$rr1,
+        sigma2=input$rr2,
+        sigma3=input$rr3,
+        period=53,
+        omega=1/(input$DurationMatImmunityDays/length.step),
+        B_m=input$B_m,
+        death.rate=0.00018,
+        lockdown=F,
+        virus_import=0,
+        I_ex=0)
+    ## Time frame
+    times      <- seq(1, input$timeframe, by = 1)
+    
+    ## Solve using ode (General Solver for Ordinary Differential Equations)
+    out_Normal <- ode(
+      y = init,
+      times = times,
+      func = msis,
+      parms = parameters_Normal
+    )   
+    
+    #    out
+    as.data.frame(out_Normal)
+  })
+  
+  
+  # Create reactive input
+  simMSIS_Intervention <- reactive({
+    init       <-
+      c(M=0,
+        S0 =  input$popsize*1000000-input$pinf,
+        I1 = input$pinf, 
+        S1 = 0,
+        I2=0,
+        S2= 0,
+        I3=0,
+        S3=0,
+        I4=0
+      )
+    ## beta: infection parameter; gamma: duration of infectiousness
+    length.step=7 # weekly
+    
+    parameters_intervention <-
       c(beta = input$R0/(input$infdur1/length.step)/input$contactrate,
         b1=input$amp,
         phi=input$phase,
@@ -54,31 +108,47 @@ server <- function(input, output) {
     times      <- seq(1, input$timeframe, by = 1)
     
     ## Solve using ode (General Solver for Ordinary Differential Equations)
-    out <- ode(
+    out_intervention <- ode(
       y = init,
       times = times,
       func = msis,
-      parms = parameters
+      parms = parameters_intervention
     )   
+    
     #    out
-    as.data.frame(out)
+    as.data.frame(out_intervention)
   })
   
-  output$distPlot <- renderPlotly({
-    out <-dataInput() %>%
+  output$Plot_Normal <- renderPlotly({
+    out_Normal <-simMSIS_Normal() %>%
       select(I1,I2,I3,I4) %>% 
       rowSums()
-    InfectionPlot <- data.frame("Total_I"=round(out),
+    InfectionPlot_Normal <- data.frame("Total_I"=round(out_Normal),
                                 date=dates)
     
-    plot_ly(InfectionPlot, type = 'scatter', mode = 'lines')%>%
+    plot_ly(InfectionPlot_Normal, type = 'scatter', mode = 'lines')%>%
       add_trace(x = ~date, y = ~Total_I, name = 'Total infected individuals')%>%
       layout(showlegend = F)%>%
       layout(legend=list(title=list(text='variable')),
              yaxis = list(title = 'Total Infected Inviduals',showgrid = FALSE,showline= T),
              xaxis = list(dtick = "M1",
                           ticklabelmode="period",showgrid = FALSE,showline= T))
-
+  })
+  
+  output$Plot_Intervention <- renderPlotly({
+    out_Intervention <-simMSIS_Intervention() %>%
+      select(I1,I2,I3,I4) %>% 
+      rowSums()
+    InfectionPlot_Intervention <- data.frame("Total_I"=round(out_Intervention),
+                                       date=dates)
+    
+    plot_ly(InfectionPlot_Intervention, type = 'scatter', mode = 'lines')%>%
+      add_trace(x = ~date, y = ~Total_I, name = 'Total infected individuals')%>%
+      layout(showlegend = F)%>%
+      layout(legend=list(title=list(text='variable')),
+             yaxis = list(title = 'Total Infected Inviduals',showgrid = FALSE,showline= T),
+             xaxis = list(dtick = "M1",
+                          ticklabelmode="period",showgrid = FALSE,showline= T))
   })
   
   # output$progressBox <- renderValueBox({
